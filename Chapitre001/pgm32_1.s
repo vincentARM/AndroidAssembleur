1071e17d439d3cec58ab011eaa45c7ca6c3035fb
vincentARM <51370770+vincentARM@users.noreply.github.com>
2021-06-05 21:01:02 +0200

    Add files via upload

--- Chapitre001/pgm32_1.s
+++ Chapitre001/pgm32_1.s
@@ -2,10 +2,20 @@
 /*  program pgm32_1.s   */
 /* affichage d'un texte simple */
 /**************************************/
+/* Constantes                         */
+/**************************************/
+.equ STDOUT,       1      @ Linux console de sortie
+.equ EXIT,         1      @ code appel système Linux
+.equ WRITE,        4      @ code appel système Linux
+/**************************************/
 /* Données initialisées               */
 /**************************************/
 .data
 szMessage:      .asciz "Pgm1 : Bonjour le monde. \n"       @ message
+/**************************************/
+/* Données non initialisées               */
+/**************************************/
+.bss
 /**************************************/
 /* Code du programme                  */
 /**************************************/
@@ -13,11 +23,11 @@ szMessage:      .asciz "Pgm1 : Bonjour le monde. \n"       @ message
 .global main 
 main:
     ldr r0,iAdrszMessage         @ adresse du message 
-    bl afficherMess             @ appel fonction d'affichage
+    bl afficherMess              @ appel fonction d'affichage
 
                                  @ fin du programme
     mov r0, #0                   @ code retour OK
-    mov r7, #1                   @ code fin LINUX 
+    mov r7, #EXIT                @ code fin LINUX 
     svc 0                        @ appel système LINUX
 
 iAdrszMessage: .int szMessage
@@ -36,8 +46,8 @@ afficherMess:
     b 1b
 2:                               @ donc ici r2 contient la longueur du message
     mov r1,r0                    @ adresse du message en r1 
-    mov r0,#1                    @ code pour écrire sur la sortie standard Linux */
-    mov r7,#4                    @ code de l appel systeme 'write' 
+    mov r0,#STDOUT               @ code pour écrire sur la sortie standard Linux */
+    mov r7,#WRITE                @ code de l appel systeme 'write' 
     svc #0                       @ appel systeme Linux
     pop {r0,r1,r2,r7,lr}         @ restaur des registres
     bx lr                        @ retour procedure
